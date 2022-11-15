1353720a98432dfe45568e37e5aa7ab2d60f5add
twwoods2 <92708497+twwoods2@users.noreply.github.com>
2022-11-15 23:54:26 +0000

    working on airport

--- src/Airport.h
+++ src/Airport.h
@@ -1,3 +1,26 @@
 #pragma once 
+#include <string>
+using namespace std;
+class Airport {
+    public: 
+    Airport(double lat, double longit, string airport_name, string id) {
+        lat_ = lat;
+        longit_ = longit;
+        airport_name_ = airport_name;
+        id_ = id;
+    }
+    Airport() {
+        lat_ = 0;
+        longit_ = 0;
+        airport_name_ = "";
+        string id_ = "";
+    }
+    string get_id() {return id_;}
+    string get_name() {return airport_name_;}
 
-Class Test {};
+    private: 
+    double lat_;
+    double longit_;
+    string airport_name_;
+    string id_;
+};
