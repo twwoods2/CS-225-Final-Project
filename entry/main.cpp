812bb3399c7f48f8a44144f69c733634e275b133
twwoods2 <92708497+twwoods2@users.noreply.github.com>
2022-11-15 23:52:30 +0000

    started airport

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,3 +1,8 @@
+#include "Airport.h"
+#include <iostream>
 int main() {
+    std::cout << "main works" << std::endl;
+    Airport test = Airport(5,3, "Ohare", "ORD");
+    std::cout << test.get_id() << std::endl;
     return 0;
 }
