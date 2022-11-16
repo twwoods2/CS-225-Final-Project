9244cb7f64e607dd393d7eba102b7f759b0af169
twwoods2 <92708497+twwoods2@users.noreply.github.com>
2022-11-16 21:31:39 +0000

    working on main

--- build/CMakeFiles/test_flights.dir/entry/test_flights.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/test_flights.dir/entry/test_flights.cpp.o: \
-  /workspaces/CS\ 225/CS-225-Final-Project/entry/test_flights.cpp
---
+++ src/Utilities.h
@@ -0,0 +1,20 @@
+#pragma once 
+
+#include <string>
+#include <vector>
+std::vector<std::string> GetSubstrs(const std::string& str,
+                                                char delimiter) {
+  size_t last = 0;
+  std::vector<std::string> substrs;
+  for (size_t i = 0; i != str.length(); ++i) {
+    if (str.at(i) == delimiter) {
+      std::string substr = str.substr(last, i - last);
+      last = i + 1;
+      substrs.push_back(substr);
+    }
+  }
+  std::string substr = str.substr(last, str.length() - last);
+  substrs.push_back(substr);
+
+  return substrs;
+}
